9967b05ebd20065c8e2e109acc636f6d75786694
Mate Toth <mztbusinessenquiries@gmail.com>
2024-09-11 10:21:49 +0100

    upvote downvote almost done

--- elprojectgrandefrontend/src/Pages/QuestionPage.jsx
+++ elprojectgrandefrontend/src/Pages/QuestionPage.jsx
@@ -4,7 +4,6 @@ import {useCookies} from "react-cookie";
 import {toast} from "react-hot-toast";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowDown, faArrowUp, faCheck} from "@fortawesome/free-solid-svg-icons";
-import error from "eslint-plugin-react/lib/util/error.js";
 
 export default function QuestionPage() {
     const [cookies] = useCookies(['user']);
@@ -123,6 +122,116 @@ export default function QuestionPage() {
         }
     }
 
+    const sendUpvote = async (id) => {
+        const res = await fetch('/api/Answers/' + id + '/upvote', {
+            method: 'PATCH',
+            headers: {
+                Authorization: cookies.user
+            }
+        })
+        await res.json();
+    }
+
+
+    const handleUpvote = async (id) => {
+        try {
+            sendUpvote(id)
+        } catch (error) {
+            console.log(error);
+            showErrorToast("Something went wrong")
+            return;
+        }
+        setAnswers(answers.map(ans => {
+            if (ans.id === id) {
+                ans.votes++;
+            }
+            return ans;
+        }))
+    }
+
+    const sendDownvote = async (id) => {
+
+        const res = await fetch('/api/Answers/' + id + '/downvote', {
+            method: 'PATCH',
+            headers: {
+                Authorization: cookies.user
+            }
+        })
+        await res.json();
+
+    }
+
+    const handleDownvote = async (id) => {
+        try {
+            sendDownvote(id);
+        } catch (e) {
+            console.log(e)
+            showErrorToast("Something went wrong")
+            return;
+        }
+        setAnswers(answers.map(ans => {
+            if (ans.id === answer.id) {
+                ans.votes--;
+            }
+            return ans;
+        }))
+
+    }
+
+    const checkIfUpvoted = (id) => {
+        return user.upvotes.some(identifier => identifier === id);
+    }
+    const checkIfDownvoted = (id) => {
+        return user.downvotes.some(identifier => identifier === id);
+    }
+
+    const handleUpvoting = (answer) => {
+        if (!checkIfUpvoted(answer.id)) {
+            handleUpvote(answer.id);
+            setUser({
+                ...user,
+                upvotes: [...user.upvotes, answer.id],
+                downvotes: user.downvotes.filter(ide => ide.id !== answer.id)
+            })
+            return;
+        }
+        sendUpvote(answer.id);
+        setAnswers(answers.map(ans => {
+            console.log(ans.id === answer.id)
+            if (ans.id === answer.id) {
+                ans.votes -= 1;
+
+            }
+            return ans;
+        }));
+        setUser({
+            ...user,
+            upvotes: user.upvotes.filter(answerId => answerId !== answer.id)
+        });
+    }
+
+    const handleDownvoting = (answer) => {
+        if (!checkIfDownvoted(answer.id)) {
+            handleDownvote(answer.id);
+            setUser({
+                ...user,
+                downvotes: [...user.downvotes, answer.id],
+                upvotes: user.upvotes.filter(ans => ans.id !== answer.id)
+            });
+            return;
+        }
+        sendDownvote(answer.id);
+        setAnswers(answers.map(ans => {
+            if (ans.id === answer.id) {
+                ans.votes++;
+            }
+            return ans;
+        }))
+        setUser({
+            ...user,
+            downvotes: user.downvotes.filter(ans => ans.id !== answer.id)
+        });
+    }
 
     return questionData && answers && user ? (
         <>
@@ -137,18 +246,27 @@ export default function QuestionPage() {
             </div>
             {
                 answers.map(answer => {
-                    console.log(questionData);
                     return (
                         <div
                             className="w-3/4 min-h-40 mt-12 p-6 bg-white rounded-lg shadow-md block m-auto">
                             <div className="flex">
                                 <div className="w-1/12 border-r-2 justify-between">
                                     <div
-                                        className="text-center text-3xl text-gray-400 hover:text-black transition block">
+                                        onClick={() => {
+                                            handleUpvoting(answer);
+                                        }}
+                                        className={'text-center text-3xl hover:text-black transition block cursor-pointer ' + (checkIfUpvoted(answer.id) ? '' : 'text-gray-400')}>
                                         <FontAwesomeIcon icon={faArrowUp}/>
                                     </div>
                                     <div
-                                        className="text-center text-3xl text-gray-400 hover:text-black transition block">
+                                        className="text-center text-3xl transition block">
+                                        {answer.votes}
+                                    </div>
+                                    <div
+                                        onClick={() => {
+                                            handleDownvoting(answer);
+                                        }}
+                                        className={"text-center text-3xl text-gray-400 hover:text-black transition block cursor-pointer " + (checkIfDownvoted(answer.id) ? '' : 'text-gray-400')}>
                                         <FontAwesomeIcon icon={faArrowDown}/>
                                     </div>
                                     {user.userName === questionData.username && !questionData.hasAccepted ? <div
