2169011a87508f2ea89f0869e025e54e0bf51789
Mate Toth <mztbusinessenquiries@gmail.com>
2024-10-27 12:00:16 +0000

    README

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://backend-server:8080/',  // Replace with your backend server URL
+                target: 'http://backend-server:8080/',
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, '')
             }
